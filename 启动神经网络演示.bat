1dd8b9213698f2b984c587770c056a4bd71a7719
taytay0612 <127462669+taytay0612@users.noreply.github.com>
2026-09-07 17:37:31 +0800

    add 启动神经网络演示.bat

---
+++ 启动神经网络演示.bat
@@ -0,0 +1,7 @@
+@echo off
+rem Neural network score model plays in Edge
+cd /d E:\deepseek\test
+set PYTHONPATH=E:\deepseek\test
+python -u -m ai_tetris.score_play_browser --model E:\deepseek\test\ai_tetris\score_net.pt --port 9222 --episodes 3 > E:\deepseek\test\ai_tetris\score_play_log.txt 2>&1
+echo score play finished
+pause
